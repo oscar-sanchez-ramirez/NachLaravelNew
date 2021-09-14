159b1cf598c9ed2f0796003b4149bd6248b755b8
oscar <oscar_sr_609@hotmail.com>
2021-09-14 17:23:53 -0500

    API Rest estable

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title>Tiros API Rest</title>
+</head>
+
+<body class="bg-dark">
+    <div class="container">
+        <div class="vh-100 m-0 row align-items-center justify-content-center">
+            <div class="col-md-12">
+                <h1 class="text-center text-white">API Rest Tiros</h1>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
