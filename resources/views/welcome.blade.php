0e8e4875c64b813fecd9ed0217503657c93aec14
oscar <oscar_sr_609@hotmail.com>
2021-09-17 16:33:02 -0500

    Se agrego talwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,14 +7,31 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Tiros API Rest</title>
 </head>
 
 <body class="bg-dark">
     <div class="container">
-        <div class="vh-100 m-0 row align-items-center justify-content-center">
-            <div class="col-md-12">
+        <div class="m-0 row justify-content-center">
+            <div class="col-md-12 mt-5">
                 <h1 class="text-center text-white">API Rest Tiros</h1>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <div class="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+                    <div class="animate-pulse flex space-x-4">
+                        <div class="rounded-full bg-blue-400 h-12 w-12"></div>
+                        <div class="flex-1 space-y-4 py-1">
+                            <div class="h-4 bg-blue-400 rounded w-3/4"></div>
+                            <div class="space-y-2">
+                                <div class="h-4 bg-blue-400 rounded"></div>
+                                <div class="h-4 bg-blue-400 rounded w-5/6"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
